b59fa27d861b05c01990def4ad8beb84afbadbf2
marcocnc <marcocancelliere56@gmail.com>
2023-03-20 15:11:13 +0100

    declarated discounts

--- js/script.js
+++ js/script.js
@@ -5,7 +5,23 @@ const price = 0.21;
 
 console.log(chilometri, age);
 
+const discount20 = "";
+const discount40 = ""
 
 // Prezzo ticket
 const priceTicket = chilometri * price;
 console.log(priceTicket);
+
+// Sconti
+if(age < 18){
+    const discount20 = priceTicket * 20 / 100;
+    const priceDiscounted20 = priceTicket - discount20;
+    console.log(priceDiscounted20);
+}
+
+if(age > 65){
+    const discount40 = priceTicket * 40 / 100;
+    const priceDiscounted40 = priceTicket - discount40;
+    console.log(priceDiscounted40);
+}
+
