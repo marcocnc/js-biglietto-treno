8394fcc0cfc31f05c16afb8913e2e9cbb4798fac
marcocnc <marcocancelliere56@gmail.com>
2023-03-20 16:43:35 +0100

    end

--- js/script.js
+++ js/script.js
@@ -1,35 +1,30 @@
 // Dichiarazione variabili
-const chilometri = prompt('Quanti chilometri vorresti percorrere?');
-const age = prompt('Quanti anni hai?');
-const price = 0.21;
+const chilometri = parseInt(prompt('Quanti chilometri vorresti percorrere?', 100));
+const age        = parseInt(prompt('Quanti anni hai?', 10));
+const price      = 0.21;
 
 console.log(chilometri, age);
 
-const discount20 = "";
-const discount40 = "";
+let discount = 0;
 
 // Prezzo ticket
-const priceTicket = chilometri * price;
+let priceTicket = chilometri * price;
 console.log(priceTicket);
 
 // Sconti
-if(age < 18){
-    const discount20 = priceTicket * 20 / 100;
-    const priceDiscounted20 = priceTicket - discount20;
-    console.log(priceDiscounted20);
+if(age < 18) {
+    discount = priceTicket * 20 / 100;
 }
 
-if(age > 65){
-    const discount40 = priceTicket * 40 / 100;
-    const priceDiscounted40 = priceTicket - discount40;
-    console.log(priceDiscounted40);
+if(age > 65) {
+    discount = priceTicket * 40 / 100;
 }
 
 // Output
 
 const message = `
-Salve viaggiatore/viaggiatrice, il tuo biglietto costa ${priceTicket}€!
+Salve viaggiatore/viaggiatrice, il tuo biglietto costa ${priceTicket - discount}€!
 `
 console.log(message);
 
-// document.getElementById('messageElement').innerHTML = message;
+document.getElementById('messageElement').innerHTML = message;
